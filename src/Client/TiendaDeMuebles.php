fc8173fcbdbae5e0b2ec188f9f8e8c9be106de8c
tonigith <91883442+toniGitH@users.noreply.github.com>
2026-01-13 19:44:38 +0100

    Modificada clase TiendaDeMuebles

--- src/Client/TiendaDeMuebles.php
+++ src/Client/TiendaDeMuebles.php
@@ -3,29 +3,74 @@
     namespace App\Client;
 
     use App\Contracts\Fabrica;
+    use App\Contracts\Mesa;
+    use App\Contracts\Silla;
+    use App\Contracts\Lampara;
     
     class TiendaDeMuebles {
 
         private Fabrica $fabrica;
+        private Silla $silla;
+        private Mesa $mesa;
+        private Lampara $lampara;
 
         public function __construct(Fabrica $fabrica) {
             $this->fabrica = $fabrica;
         }
 
-        public function mostrarConjunto() {
-            $estilo = $this->fabrica->getEstilo();
-            echo "Abriendo catálogo de muebles de estilo $estilo\n";
-            echo "---------------------\n";
-            $this->exponerConjunto();
+        public function pedirConjuntoAFabrica() {
+            $this->silla = $this->fabrica->fabricarSilla();
+            $this->mesa = $this->fabrica->fabricarMesa();
+            $this->lampara = $this->fabrica->fabricarLampara();
         }
 
-        public function exponerConjunto() {
-            $silla = $this->fabrica->fabricarSilla();
-            $mesa = $this->fabrica->fabricarMesa();
-            $lampara = $this->fabrica->fabricarLampara();
+        public function venderConjunto() {
+            $estilo = $this->fabrica->getEstilo();
+            echo "\n";
+            echo "Vendido conjunto de muebles de estilo $estilo\n";
+            echo "---------------------\n";
+            $this->pedirConjuntoAFabrica();
+            echo $this->silla->desplegar();
+            echo $this->mesa->desplegar();
+            echo $this->lampara->encender();
+        }
 
-            echo $silla->desplegar();
-            echo $mesa->desplegar();
-            echo $lampara->encender();
+        public function pedirSillaAFabrica() {
+            $this->silla = $this->fabrica->fabricarSilla();
+        }
+
+        public function venderSilla() {
+            $estilo = $this->fabrica->getEstilo();
+            echo "\n";
+            echo "Vendida silla de estilo $estilo\n";
+            echo "---------------------\n";
+            $this->pedirSillaAFabrica();
+            echo $this->silla->desplegar();
+        }
+
+        public function pedirMesaAFabrica() {
+            $this->mesa = $this->fabrica->fabricarMesa();
+        }
+
+        public function venderMesa() {
+            $estilo = $this->fabrica->getEstilo();
+            echo "\n";
+            echo "Vendida mesa de estilo $estilo\n";
+            echo "---------------------\n";
+            $this->pedirMesaAFabrica();
+            echo $this->mesa->desplegar();
+        }
+
+        public function pedirLamparaAFabrica() {
+            $this->lampara = $this->fabrica->fabricarLampara();
+        }
+
+        public function venderLampara() {
+            $estilo = $this->fabrica->getEstilo();
+            echo "\n";
+            echo "Vendida lampara de estilo $estilo\n";
+            echo "---------------------\n";
+            $this->pedirLamparaAFabrica();
+            echo $this->lampara->encender();
         }
     }
